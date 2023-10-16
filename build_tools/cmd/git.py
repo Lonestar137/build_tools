b08c883e42e00418346fb20dd86332dfa189e878
Garrett Jones <garretcj2010@hotmail.com>
2023-10-15 19:52:26 -0500

    Added mode and build function.

--- build_tools/cmd/git.py
+++ build_tools/cmd/git.py
@@ -10,8 +10,8 @@ class GitRepo:
         self.uri = uri
         self.git_cmd = "git"
         self.git_cmd_args = []
-        self.cmds_to_run = []
         self.repo_name = uri.split('/')[-1][:-4]
+        self.cloned_state = False
 
     def clone(self, folder_name: str = None):
         cmd = None
@@ -20,53 +20,41 @@ class GitRepo:
             cmd = [self.git_cmd, "clone", self.uri, folder_name]
         else:
             cmd = [self.git_cmd, "clone", self.uri]
-        self.cmds_to_run.append(cmd)
+        subprocess.run(cmd)
         if self.submodules:
             sub_cmd = [self.git_cmd, "submodules",
                        "update", "--init", "--recursive"]
-            self.cmds_to_run.append(sub_cmd)
+            subprocess.run(sub_cmd)
+        self.cloned_state = True
         return self
 
 
 class PythonGitRepo(GitRepo):
     def __init__(self, uri, submodules=False):
-        super.__init__(self, uri, submodules)
-        self.mode = "install"
+        super().__init__(uri, submodules)
         self.__venv = None
+        self.cmds_to_run = []
 
-        self.python_cmd = None
-        if platform.os == "nt":
-            self.python_cmd = ["py", "-3"]
-        else:
-            self.python_cmd = ["/usr/bin/env", "python"]
+        self.python_cmd = ["python", "-m"]
+        # if platform.os == "nt":
+        #     self.python_cmd = ["py", "-3"]
+        # else:
+        #     self.python_cmd = ["/usr/bin/env", "python"]
 
         self.python_cmd_args = []
+        self.modes = ["pip", "wheel"]
 
     def set_requirements_file(self, file_path: str = "requirements.txt"):
         self.python_cmd_args.extend(["--requirement", file_path])
         return self
 
     def set_editable(self):
-        self.python_cmd_args.append("-e")
+        self.python_cmd_args.append("--editable")
         return self
 
     def set_find_links(self, dest):
         self.python_cmd_args.extend(["--find-links", dest])
         return self
-
-    def use_venv(self, path: str):
-        self.__venv = path
-        if platform.os == "nt":
-            # TODO: detect if cmd prompt or powershell.
-            self.cmds_to_run.append(
-                Path(f"{path}/Scripts/Activate.ps").__str__())
-        else:
-            self.cmds_to_run.append(
-                Path(f"source {path}/bin/activate").__str__())
-
-        cmd = self.python_cmd.copy()
-        cmd.extend(["-m", "venv", path])
-        self.cmds_to_run.append(cmd)
 
     def wheels(self, dest: str):
         cmd = self.python_cmd.copy()
@@ -78,13 +66,27 @@ class PythonGitRepo(GitRepo):
         return self
 
     def install(self):
+        cmd = self.python_cmd.copy()
+        if "--requirement" in self.python_cmd_args:
+            cmd.extend(["pip", "install"])
+        else:
+            cmd.extend(["pip", "install", "."])
+        self.cmds_to_run.append(cmd + self.python_cmd_args)
         return self
 
     def build(self):
-        print("build . . .")
-        print(self.cmds_to_run)
-        # for cmd in self.cmds_to_run:
-        #     subprocess.run(cmd)
+        if not self.cloned_state:
+            self.clone()
+        print("Building with provided commands. . .")
+        ran = False
+        for cmd in self.cmds_to_run:
+            print(cmd)
+            for mode in self.modes:
+                if mode in cmd:
+                    subprocess.run(cmd)
+                    ran = True
+        if not ran:
+            print(f"No valid command was provided: {self.cmds_to_run}")
 
 
 if __name__ == "__main__":
