0672f8e37be99a4b4f54f0c345eeeac0d01a1a0a
Garrett Jones <garretcj2010@hotmail.com>
2023-10-28 19:06:44 -0500

    Redo some pathing in the tests and add cleanup

--- tests/cmd/test_git.py
+++ tests/cmd/test_git.py
@@ -4,61 +4,60 @@ from build_tools.context.context_dir import Dir
 from build_tools.cmd.git import GitRepo, PythonGitRepo
 
 
-tmp_dir = Path("@tmp/test_git")
+test_dir = Path("@tmp/test_git")
 
 
-with Dir(tmp_dir, remove_after=True):
-    test_dir = f"{tmp_dir}"
+def test_create_dir():
+    with Dir(f"{test_dir}/directory"):
+        with Dir("subdir"):
+            assert Path(
+                "../subdir").exists(), "Failed to create sub directory."
 
-    def test_create_dir():
-        with Dir(f"{test_dir}/directory"):
-            with Dir("subdir"):
-                assert Path(
-                    "../subdir").exists(), "Failed to create sub directory."
+    assert Path(
+        f"{test_dir}/directory").exists(), "Failed to create (multi)directory"
 
-        assert Path(
-            f"{test_dir}/directory").exists(), "Failed to create (multi)directory"
 
-    def test_git_repo():
-        with Dir(f"{test_dir}"):
-            req_repo = GitRepo("https://github.com/zpqrtbnk/test-repo.git")
-            req_repo.clone()
+def test_git_repo():
+    with Dir(f"{test_dir}"):
+        req_repo = GitRepo("https://github.com/zpqrtbnk/test-repo.git")
+        req_repo.clone()
 
-            req_repo.clone("renamed-repo")
+        req_repo.clone("renamed-repo")
 
-        assert Path(
-            f"{test_dir}/test-repo").exists(), "Cloned repo folder does not exist."
-        assert Path(
-            f"{test_dir}/renamed-repo").exists(), "Cloned repo was not renamed."
-        assert Path(
-            f"{test_dir}/test-repo/.git").exists(), "Cloned repo was not a git repo."
-        assert Path(
-            f"{test_dir}/renamed-repo/.git").exists(), "Cloned repo  was not a git repo."
+    assert Path(
+        f"{test_dir}/test-repo").exists(), "Cloned repo folder does not exist."
+    assert Path(
+        f"{test_dir}/renamed-repo").exists(), "Cloned repo was not renamed."
+    assert Path(
+        f"{test_dir}/test-repo/.git").exists(), "Cloned repo was not a git repo."
+    assert Path(
+        f"{test_dir}/renamed-repo/.git").exists(), "Cloned repo  was not a git repo."
 
-    def test_python_repo():
-        with Dir(Path(test_dir) / "python_test"):
-            expected_wheels = ["peppercorn-",
-                               "sampleproject-]"]
-            sample = PythonGitRepo("https://github.com/pypa/sampleproject.git")
-            sample.clone("sample")
-            sample.wheels("wheelhouse")
-            sample.build()
 
-            assert Path(f"wheelhouse").exists()
-            assert Path(f"sample/.git").exists()
-            whls = Path(f"wheelhouse").glob("*.whl")
-            i = 0
-            # for w in whls:
-            #     assert expected_wheels[i] in w, "Incorrect or missing wheels."
-            #     i += 1
+def test_python_repo():
+    with Dir(Path(test_dir) / "python_test"):
+        expected_wheels = ["peppercorn-",
+                           "sampleproject-]"]
+        sample = PythonGitRepo("https://github.com/pypa/sampleproject.git")
+        sample.clone("sample")
+        sample.wheels("wheelhouse")
+        sample.build()
 
-        assert Path(f"{test_dir}/python_test").exists()
+        assert Path(f"wheelhouse").exists()
+        assert Path(f"sample/.git").exists()
+        whls = Path(f"wheelhouse").glob("*.whl")
+        i = 0
+        # for w in whls:
+        #     assert expected_wheels[i] in w, "Incorrect or missing wheels."
+        #     i += 1
 
-    # TODO: Prevent making bad directory names.  I.e. if you pass a function to WithDir
-    # TODO: Test that Path and str are treated the same/correctly.
+    assert Path(f"{test_dir}/python_test").exists()
 
-    def test_cleanup():
-        with Dir(f"test", remove_after=True):
-            pass
+# TODO: Prevent making bad directory names.  I.e. if you pass a function to WithDir
+# TODO: Test that Path and str are treated the same/correctly.
 
-        assert not Path("test").exists()
+
+def test_cleanup():
+    with Dir(test_dir, remove_after=True):
+        pass
+    assert not Path(test_dir).exists()
--- tests/context/test_venv.py
+++ tests/context/test_venv.py
@@ -1,19 +1,20 @@
 import os
 import platform
 import subprocess
+import pytest
 from pathlib import Path
 from build_tools.context.venv import Venv
 from build_tools.context.context_dir import Dir
 
 
-tmp_dir = Path("@tmp")
 test_venv = "test_venv"
+test_dir = Path("@tmp") / test_venv
 
 
-# with Dir(tmp_dir / "test_venv", remove_after=False):
+# with Dir(test_dir / "test_venv", remove_after=False):
 
 def test_context():
-    with Dir(tmp_dir / "test_venv", remove_after=False):
+    with Dir(test_dir, remove_after=False):
         with Venv(test_venv):
             if platform.os == "nt":
                 assert Path(f"{test_venv}/Scripts").exists()
@@ -21,14 +22,14 @@ def test_context():
                 assert Path(f"{test_venv}/bin").exists()
 
 
+@pytest.mark.skip
 def test_reuse_venv():
-    with Dir(tmp_dir / "test_venv", remove_after=False):
+    with Dir(test_dir, remove_after=False):
         venv = Venv("venv")
         test_venv_path = Path(test_venv).absolute()
 
         def test_activate():
             venv.activate_venv()
-            breakpoint()
 
             assert test_venv_path.__str__() == os.environ["VIRTUAL_ENV"]
             if platform.os == "nt":
@@ -36,44 +37,49 @@ def test_reuse_venv():
             else:
                 assert f"{test_venv_path}/bin" in os.environ["PATH"]
 
-        # def test_deactivate():
-        #     venv.deactivate_venv()
-        #     try:
-        #         os.environ["VIRTUAL_ENV"]
-        #     except KeyError:
-        #         assert True, "Env variable successfully deinitialized."
-        #     except:
-        #         assert False, "Got unexpected exception"
+        def test_deactivate():
+            venv.deactivate_venv()
+            try:
+                os.environ["VIRTUAL_ENV"]
+            except KeyError:
+                assert True, "Env variable successfully deinitialized."
+            except:
+                assert False, "Got unexpected exception"
 
         test_activate()
-        # test_deactivate()
+        test_deactivate()
 
 
 def test_install():
     requirements = ["qasync", "pytest"]
-    with Dir(tmp_dir / "test_venv", remove_after=False):
-        with Venv("venv", py_modules=requirements) as venv:
+    with Dir(test_dir, remove_after=False):
+        with Venv(test_venv, py_modules=requirements) as venv:
             extras = [
                 "requests",
                 "invoke"
             ]
 
             bin = None
+            lib = f"{test_venv}/lib/"
             if platform.os == "nt":
-                bin = Path("venv/Scripts/")
+                bin = Path(test_venv + "/Scripts/")
             else:
-                bin = Path("venv/bin/")
+                bin = Path(test_venv + "/bin/")
 
             assert not Path(f"{bin}/invoke").exists()
             assert Path(f"{bin}/pytest").exists()
+            # assert Path(f"venv/lib/").exists()
 
             venv.install(extras)
             assert Path(f"{bin}/invoke").exists()
-
-            # subprocess.run(["invoke", "--help"], stdout=subprocess.PIPE)
-            subprocess.run(["invoke", "--help"])
 
 
 def test_venv_install():
 
     pass
+
+
+def test_cleanup():
+    with Dir(test_dir, remove_after=True):
+        pass
+    assert not test_dir.exists()
